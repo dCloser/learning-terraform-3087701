d43399c8fab9fc5579e28b0d6210ae4c07dbb2bd
dCloser <oghenefejiroc@gmail.com>
2023-01-14 04:14:46 -0600

    new vpc

--- main.tf
+++ main.tf
@@ -14,8 +14,21 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-2a", "eu-west-2b", "eu-west-3c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
 resource "aws_instance" "web" {
@@ -23,17 +36,17 @@ resource "aws_instance" "web" {
   instance_type = "t3.nano"
   vpc_security_group_ids = [module.blog_sg.security_group_id]
   tags = {
-    Name = "Learning Terraform"
+    Name = "FejiDevOps Terraform"
   }
 }
 
 module "blog_sg" {
   source = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
+  
+  
   name = "blog_new"
-
-
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.vpc.public_subnets[0]
 
 
   ingress_rules   = ["http-80-tcp", "https-443-tcp"]
